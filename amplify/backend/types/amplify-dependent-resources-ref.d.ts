754423a3f19e0eb6a63e00b1f1b0338be48cccec
sabina-sudo <sabrine.bouchiha@gmail.com>
2024-09-15 18:54:35 +0100

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp4sabrine": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
